69b9c7374cce4fd3a107dc53010e7d5e5b14ca3d
nahurup <nahuelruppel10@gmail.com>
2021-10-12 14:56:40 -0300

    requisitos juradoExigente

--- src/produ/JuradoExigente.java
+++ src/produ/JuradoExigente.java
@@ -2,6 +2,9 @@ package produ;
 import java.util.ArrayList;
 
 import produ.criterios.Criterio;
+import produ.criterios.CriterioGenero;
+import produ.criterios.CriterioIdioma;
+import produ.criterios.CriterioInstrumento;
 
 public class JuradoExigente extends Jurado {
 	//Atributos
@@ -13,9 +16,19 @@ public class JuradoExigente extends Jurado {
     }
 	
 	//Metodos
-    public void agregarParticipante(Participante p, Criterio criterio) {
-    	if(!equipo.contains(p) && criterio.cumple(p)) {
-    		equipo.add(p);
+    public void agregarParticipante(Participante p, String instrumento, String idioma, String genero) {
+    	if(!equipo.contains(p)) {
+    		
+        	Criterio crit_instrumento = new CriterioInstrumento(instrumento);
+        	Criterio crit_idioma = new CriterioIdioma(idioma);
+        	Criterio crit_genero = new CriterioGenero(genero);
+        	
+        	if (crit_instrumento.cumple(p) && crit_idioma.cumple(p) && crit_genero.cumple(p)) {
+        		equipo.add(p);
+        	} else {
+        		System.out.println("No cumple requisitos.");
+        	}
+        	
     	}
     }
 }
